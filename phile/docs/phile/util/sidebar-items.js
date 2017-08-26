72c38f14030674df5e101176877fd44d33eaf535
Árpád Goretity <h2co3@h2co3.org>
2017-08-26 20:57:54 +0200

    Updated PHiLe documentation to v0.1.1

--- phile/docs/phile/util/sidebar-items.js
+++ phile/docs/phile/util/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["grapheme_count",""],["unescape_string_literal",""]],"static":[["COLOR",""],["PACKAGE_INFO",""]],"struct":[["Color",""],["PackageInfo",""],["RcCell",""],["WkCell",""]]});
+initSidebarItems({"fn":[["grapheme_count",""],["grapheme_count_by",""],["unescape_string_literal",""]],"static":[["COLOR",""],["PACKAGE_INFO",""]],"struct":[["Color",""],["PackageInfo",""],["RcCell",""],["WkCell",""]]});
