81f269f9ed6881c206adfd906c1591d4efa13b9b
Árpád Goretity <h2co3@h2co3.org>
2017-08-30 12:37:11 +0200

    Updated with SQIR and SQIRGen docs

--- phile/docs/phile/sqirgen/sidebar-items.js
+++ phile/docs/phile/sqirgen/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["generate_sqir",""]]});
+initSidebarItems({"fn":[["generate_sqir","Takes a borrowed AST, performs various kinds of semantic analysis on it, and if it represents a correct program, then outputs the corresponding SQIR code."]]});
